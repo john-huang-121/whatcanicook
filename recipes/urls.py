eed6634487b07030c7f4bf2703e4d6ec57f40e2d
John Huang <johnhuang121@gmail.com>
2025-04-17 13:35:41 -0700

    creates recipes models and makes migration

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # path("<int:recipe_id>/", views.detail, name="detail"),
+    # path("<int:recipe_id>/results", views.results, name="results"),
+    # path("<int:recipe_id>/edit", views.edit, name="edit"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Recipe
+
 # Create your views here.
 def index(request):
+    recipes = Recipe.objects.all()
+    
     return HttpResponse("Hello, world! You're in recipes.")
