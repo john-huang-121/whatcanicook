2e5764d3b0441311e506d9785c284d738d3e0e54
john-huang-121 <40417967+john-huang-121@users.noreply.github.com>
2025-08-08 23:04:40 -0700

    adds login and logout to the navbar

--- whatcanicook/settings.py
+++ whatcanicook/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'splash.apps.SplashConfig',
     'recipes.apps.RecipesConfig',
     'users',
